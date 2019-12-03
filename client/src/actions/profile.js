a046c7deebf6a0617512a40f137f13e6a23ee33f
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-12-03 19:28:10 +0530

    Profile by ID and github repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,12 +7,12 @@ import {
   UPDATE_PROFILE,
   ACCOUNT_DELETE,
   CLEAR_PROFILE,
-  GET_PROFILES
+  GET_PROFILES,
+  GET_REPOS
 } from "./types";
 
 //Get current users profile
 export const getCurrentProfile = () => async dispatch => {
-  dispatch({ type: CLEAR_PROFILE });
   try {
     const res = await axios.get("/api/profile/me");
 
@@ -34,11 +34,54 @@ export const getCurrentProfile = () => async dispatch => {
 //Get all profiles
 
 export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   try {
     const res = await axios.get("/api/profile");
 
     dispatch({
       type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      }
+    });
+  }
+};
+
+//Get profile by ID
+
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userID}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      }
+    });
+  }
+};
+
+//Get Github repos
+
+export const getGithubRepos = username => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
       payload: res.data
     });
   } catch (err) {
