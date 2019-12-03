40ea2ae29bb03745c31ffc26bbee16d76d365f66
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-12-03 21:05:56 +0530

    Display all profiles

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 
 //Redux
 import { loadUser } from "./actions/auth";
@@ -61,6 +62,7 @@ const App = () => {
                 path="/add-education"
                 component={AddEducation}
               />
+              <PrivateRoute exact path="/profiles" component={Profiles} />
             </Switch>
           </section>
         </Fragment>
